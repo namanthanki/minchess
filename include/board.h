8a6985c8fa1e535c199833e7bc774cc91d7776bc
Naman Thanki <stuniz7855@gmail.com>
2024-07-06 22:00:53 +0530

    Missing definitions

--- include/board.h
+++ include/board.h
@@ -3,6 +3,26 @@
 
 #include "types.h"
 
+// File masks
+#define FILE_A_BB 0x0101010101010101ULL
+#define FILE_B_BB 0x0202020202020202ULL
+#define FILE_C_BB 0x0404040404040404ULL
+#define FILE_D_BB 0x0808080808080808ULL
+#define FILE_E_BB 0x1010101010101010ULL
+#define FILE_F_BB 0x2020202020202020ULL
+#define FILE_G_BB 0x4040404040404040ULL
+#define FILE_H_BB 0x8080808080808080ULL
+
+// Rank masks
+#define RANK_1_BB 0x00000000000000FFULL
+#define RANK_2_BB 0x000000000000FF00ULL
+#define RANK_3_BB 0x0000000000FF0000ULL
+#define RANK_4_BB 0x00000000FF000000ULL
+#define RANK_5_BB 0x000000FF00000000ULL
+#define RANK_6_BB 0x0000FF0000000000ULL
+#define RANK_7_BB 0x00FF000000000000ULL
+#define RANK_8_BB 0xFF00000000000000ULL
+
 typedef struct
 {
     unsigned int white_king_side : 1;
@@ -38,8 +58,11 @@ int is_square_occuppied(const board *b, enum square s);
 
 void board_move_piece(board *b, enum square from, enum square to);
 
-void is_board_in_check(const board *b, enum color c);
-
+bitboard knight_attacks(enum square s);
+bitboard bishop_attacks(enum square s, bitboard occupied);
+bitboard rook_attacks(enum square s, bitboard occupied);
+bitboard queen_attacks(enum square s, bitboard occupied);
+bitboard king_attacks(enum square s);
 bitboard board_get_attacked_squares(const board *b, enum color c);
 
 #endif
